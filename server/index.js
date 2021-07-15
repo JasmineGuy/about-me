5aa73a36afb52def2f7a79710bbeb0ad4e15401b
JasmineGuy <guyjasmine.c@gmail.com>
2021-07-15 12:35:46 -0500

    new changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-
-const app = express()
 const path = require('path')
+const app = express()
+
 
 const port = process.env.PORT || 4005
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '..index.html'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 app.listen(port, () => {
